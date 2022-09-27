e394496ef2905d6a8248a2778a768d593225a361
CoelhoRafael <rafaelcoelho3110@gmail.com>
2022-09-26 21:24:03 -0300

    FEAT: ajustes address

--- src/main/kotlin/sweet/apisweetstore/model/Product.kt
+++ src/main/kotlin/sweet/apisweetstore/model/Product.kt
@@ -1,14 +1,14 @@
 package sweet.apisweetstore.model
 
-import javax.persistence.Entity
-import javax.persistence.ManyToOne
-import javax.persistence.OneToOne
-import javax.persistence.Table
+import java.util.*
+import javax.persistence.*
 
 
 @Entity
 @Table(name = "tb_product")
 data class Product(
+        @Id
+        val uuid: String? = UUID.randomUUID().toString(),
         val name: String? = "",
         val description: String? = "",
         val price: Double,
